ae1e2258c89d3fb147d1df7943df8b67f8d45e48
Yanet Gomez <48337148+ygharo@users.noreply.github.com>
2019-06-05 22:07:50 -0500

    Update Univ_Elevator.java
    Adding notes

--- src/Univ_Elevator.java
+++ src/Univ_Elevator.java
@@ -1,55 +1,81 @@
 
 import java.util.Scanner;
 
+//The following code executes the University Elevator always returning it to the basement level.
+//Open loop to always scan the users destination, one after the other.
 public class Univ_Elevator {
 	boolean isOpen = false; // Class variables
 	int currentFloor = 0; // Elevator starts at basement level
+	int returnedFloor = currentFloor;
 
 	// Method to be executed when called
-	int Up(int toFloor) { // toFloor is a variable to pass parameters into when method is called. Use
-							// scanner for parameters
-		if ((toFloor > currentFloor) && (toFloor < 2)) {
+	// Method to be executed when called
+	int Direction(int toFloor) { // toFloor is a variable to pass parameters into when method is called. Use
+									// scanner for parameters. This loop always returns the elevator to the basement
+									// level.
 
-			u();   //calling another method of the class
+		if ((toFloor > currentFloor) && (toFloor < 2))
 
-		} else if (toFloor == 2) {
-			u2(); 
+		{
+
+			u(); // calling another method of the class
+
+		} 
+		else {
+			u2();
 		}
-		else
-			System.out.println("Staying at " + currentFloor + " floor"); //user error
 
-		return currentFloor;  //return same type as method
+		if ((toFloor < currentFloor) && (toFloor > 0)) {
+			d();
+
+		} 
+		else {
+			d2();
+		}
+
+		return currentFloor; // return same type as method
 
 	}
 
-	private void up() { // private method of this class
+	void up() { 
 		if (currentFloor < 2) {
 			currentFloor++;
+			System.out.println("Going to the,,, " + currentFloor + " floor");
+
+		} else {
+			System.out.println("Staying at the" + currentFloor + " floor");
+
+		}
+	}
+
+	void openDoor() { 
+		isOpen = true;
+		System.out.println("Door is opening");
+
+		// This makes the code wait for 2 seconds (chapter 11)
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		// End of wait code.
+	}
+
+	void down() {
+		if (currentFloor > 0) {
+			currentFloor--;
 			System.out.println("Going to,,, " + currentFloor);
 		} else {
 			System.out.println("Staying at " + currentFloor);
 		}
 	}
 
-	private void openDoor() {   //door method
-		isOpen = true;
-		System.out.println("Door opened");
-		
-		// This makes the code wait for 2 seconds (chapter 11)
-				try {
-					Thread.sleep(2000); 
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				// End of wait code.
-	}
-
-	private void closeDoor() {
+	void closeDoor() {
 		isOpen = false;
-		System.out.println("Door closed");
+		System.out.println("Door is closing");
 	}
 
-	public void u() { //method to be used in 'main' which calls the local private method up()
+	public void u() { // method to be used in 'main' which calls the local method up() which has the 'mecanica'
 		up();
 		openDoor();
 		closeDoor();
@@ -57,18 +83,14 @@ public class Univ_Elevator {
 
 	public void u2() {
 		up();
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		up();
 		openDoor();
 		closeDoor();
-	}
-
-	private void down() {  
-		if (currentFloor > 0) {
-			currentFloor--;
-			System.out.println("Going to,,, " + currentFloor);
-		} else {
-			System.out.println("Staying at " + currentFloor);
-		}
 	}
 
 	public void d() {
@@ -79,6 +101,11 @@ public class Univ_Elevator {
 
 	public void d2() {
 		down();
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		down();
 		openDoor();
 		closeDoor();
@@ -87,17 +114,17 @@ public class Univ_Elevator {
 	public static void main(String[] args) // main method
 	{
 
-		System.out.print("Which floor are you going to?\n\t");
+		System.out.print("Which floor are you going to: 0 1 2?\n\t");
 		Scanner scan = new Scanner(System.in);
 
 		int choice = scan.nextInt(); // will scan what floor the user will go to, pass it to toFloor
 
-		
 		Univ_Elevator E = new Univ_Elevator(); // creates an object ( univ elevator) of the class Elevator
 
-	 
-	    int returnedFLoor = E.Up(choice); // call method
-		
-		
+		E.Direction(choice); // call method
+
+		while ((choice = scan.nextInt()) != 0) {
+			E.Direction(choice);
+		}
 	}
 }
